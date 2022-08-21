fcb624a3dd5c678673899692da61a25e26d4fe07
Srijan Bhattacharyya <78896721+RohitBhattacharyya@users.noreply.github.com>
2022-08-21 09:37:45 +0530

    Update dbms_sql.py

--- dbms_sql.py
+++ dbms_sql.py
@@ -1,36 +1,62 @@
-import sqlite3 as sql, sys, os
+import sqlite3 as sql, os
 
 
 class DBMS:
-    def __init__(self, db_path: os.path, ip: str, port: int, user: str, password: str, db: str):
+    def __init__ (self, db_path: os.path, tableName: str) -> None:
         self.db_path = db_path
-        self.ip = ip
-        self.port = port
-        self.user = user
-        self.password = password
-        self.db = db
+        self.tableName = tableName
+
         self.conn = None
         self.cursor = None
 
-    def connect(self):
-        self.conn = sql.connect(self.db_path)
-        self.cursor = self.conn.cursor()
+    def connect (self) -> None:
+        # if self.dp_path exists, connect to it, else create it
+        if os.path.exists (self.db_path):
+            self.conn = sql.connect (self.db_path)
+            self.cursor = self.conn.cursor ()
 
-    def close(self):
-        self.conn.close()
+        else:
+            self.conn = sql.connect (self.db_path)
+            self.cursor = self.conn.cursor ()
+            self.create_table ()
 
-    def create_table (self):
-        self.cursor.execute ('''CREATE TABLE IF NOT EXISTS items (
+    def create_table (self) -> None:
+        '''name TEXT, URL TEXT, priceLimit TEXT, dateLimit TEXT OR BOOL'''
+
+        self.cursor.execute (f'''CREATE TABLE IF NOT EXISTS {self.tableName} (
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 name TEXT,
                                 URL TEXT,
                                 priceLimit TEXT,
-                                dateLimit,
+                                dateLimit TEXT OR BOOL)
                             ''')
 
-    def insert_data (self, data: dict):
+    def insert_data (self, data: dict) -> None:
         '''name TEXT, URL TEXT, priceLimit TEXT, dateLimit TEXT OR BOOL'''
 
         self.cursor.execute ('''INSERT INTO items (name, URL, priceLimit, dateLimit)
                                 VALUES (?, ?, ?, ?)''', (data ['name'], data ['URL'], data ['priceLimit'], data ['dateLimit']))
-        self.conn.commit()
+        self.conn.commit ()
+
+    def update_data (self, id: int, data: dict) -> None:
+        self.cursor.execute (f'''UPDATE items SET name = ?, URL = ?, priceLimit = ?, dateLimit = ? WHERE id = {id}''', (data ['name'], data ['URL'], data ['priceLimit'], data ['dateLimit']))
+        self.conn.commit ()
+
+    def get_all_data (self) -> list:
+        self.cursor.execute ('''SELECT * FROM items''')
+        return self.cursor.fetchall ()
+
+    def get_data (self, id: int) -> list:
+        self.cursor.execute (f'''SELECT * FROM items WHERE id = {id}''')
+        return self.cursor.fetchone ()
+
+    def delete_data (self, id: int) -> None:
+        self.cursor.execute (f'''DELETE FROM items WHERE id = {id}''')
+        self.conn.commit ()
+
+    def delete_all_data (self) -> None:
+        self.cursor.execute ('''DELETE FROM items''')
+        self.conn.commit ()
+
+    def close (self) -> None:
+        self.conn.close ()
